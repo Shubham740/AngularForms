0d1fa24fa291cf9f1f0b2a505b521c20553d3de5
shubham740 <shubhampusa740@gmail.com>
2020-12-27 19:19:47 +0530

    implement pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,12 @@ import {FormGroupName, FormControl, FormGroup,Validators} from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyForms';
-    onSubmit(data: object){
+  title = 'Welcome to angular 7';
+  money = 23333.34;
+  date = new Date();
+    nameObject = {name:'Shubham', Age:25, height:'5 Foot 11 Inch',yearsOfEx: 4}
+
+  onSubmit(data: object){
       console.log('data=>>>', data)
     }
 }
